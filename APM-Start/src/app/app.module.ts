44df5f31f6b8aed9b63f28cb73e311601ef78565
Lige-w <lige.williamson@gmail.com>
2019-10-29 12:11:59 -0400

    added routing to product detail page

--- APM-Start/src/app/app.module.ts
+++ APM-Start/src/app/app.module.ts
@@ -3,14 +3,34 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import {ProductListComponent} from './products/product-list.component';
+import {FormsModule} from '@angular/forms';
+import {ConvertToSpacesPipe} from './shared/convert-to-spaces.pipe';
+import {StarComponent} from './shared/star.component';
+import {HttpClientModule} from '@angular/common/http';
+import { ProductDetailComponent } from './products/product-detail.component';
+import {RouterModule} from '@angular/router';
+import {WelcomeComponent} from './home/welcome.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductListComponent
+    ProductListComponent,
+    ConvertToSpacesPipe,
+    StarComponent,
+    ProductDetailComponent,
+    WelcomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    RouterModule.forRoot([
+      { path: 'products', component: ProductListComponent },
+      { path: 'products/:id', component: ProductDetailComponent },
+      { path: 'welcome', component: WelcomeComponent },
+      { path: '', redirectTo: 'welcome', pathMatch: 'full' },
+      { path: '**', redirectTo: 'welcome', pathMatch: 'full' }
+    ])
   ],
   bootstrap: [AppComponent]
 })
